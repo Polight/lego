a6fb0e18ce10a0f847dc87953ee4d7aa65a5e87c
Vincent Agnano <vincent.agnano@scopyleft.fr>
2018-11-10 22:33:55 +0100

    attributes are converted into context properties

--- dist/lego.js
+++ dist/lego.js
@@ -2,22 +2,27 @@ class Component extends HTMLElement {
   constructor(...args) {
     super(...args)
     this.shadow = this.attachShadow({mode: 'open'})
-    this.extended = Object.assign({ template: '', style: '', context: {} }, this.extended)
+    this.properties = Object.assign({ template: '', style: '', context: {} }, this.properties)
+    this.attributesToContext()
     this.render()
   }
 
-  get context() {return {}}
+  attributesToContext() {
+    const attributes = {}
+    Array.from(this.attributes).map(attr => attributes[attr.name] = attr.value)
+    Object.assign(this.properties.context, attributes)
+  }
 
   render(ctx = {}) {
-    const context = Object.assign(this.extended.context, ctx)
-    const templater = new Function(`return \`${this.extended.style}${this.extended.template}\``)
+    const context = Object.assign(this.properties.context, ctx)
+    const templater = new Function(`return \`${this.properties.style}${this.properties.template}\``)
     this.shadow.innerHTML = templater.apply(context)
   }
 }
 
 export default function defineComponent(name, props) {
   class C extends Component {}
-  C.prototype.extended = props
+  C.prototype.properties = props
 
   customElements.define(name, C)
   return C
