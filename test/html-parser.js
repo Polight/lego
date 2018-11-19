abf8c1326d0118ca3971fd5ab5d6c7ee51c5b2fa
Vincent Agnano <vincent.agnano@scopyleft.fr>
2018-11-19 15:28:58 +0100

    unified syntax to get a semicolumn as Lego attributes

--- test/html-parser.js
+++ test/html-parser.js
@@ -30,41 +30,41 @@ describe('HTMLParser', () => {
 
   describe('#parseAttributeIf()', () => {
     it('should keep node when "if" attr is true', () => {
-      const dom = HTMLParser.htmlToDom('<p><a>link</a><b if="true">Hey</b></p>')
+      const dom = HTMLParser.htmlToDom('<p><a>link</a><b :if="true">Hey</b></p>')
       assert.equal(HTMLParser.parseAttributeIf(dom).innerHTML, '<p><a>link</a><b>Hey</b></p>')
     })
     it('should remove node when "if" attr is false', () => {
-      const dom = HTMLParser.htmlToDom('<p><a>link</a><b if="false">Hey</b></p>')
+      const dom = HTMLParser.htmlToDom('<p><a>link</a><b :if="false">Hey</b></p>')
       assert.equal(HTMLParser.parseAttributeIf(dom).innerHTML, '<p><a>link</a></p>')
     })
     it('should preserve deep tree node when true', () => {
-      const dom = HTMLParser.htmlToDom('<p><a><b if="true">bold link</b></a></p>')
+      const dom = HTMLParser.htmlToDom('<p><a><b :if="true">bold link</b></a></p>')
       assert.equal(HTMLParser.parseAttributeIf(dom).innerHTML, '<p><a><b>bold link</b></a></p>')
     })
     it('should remove deep tree node when false', () => {
-      const dom = HTMLParser.htmlToDom('<p><a><b if="false">bold link</b></a></p>')
+      const dom = HTMLParser.htmlToDom('<p><a><b :if="false">bold link</b></a></p>')
       assert.equal(HTMLParser.parseAttributeIf(dom).innerHTML, '<p><a></a></p>')
     })
   })
 
   describe('#parseAttributeBoolean()', () => {
     it('should create valid attribute when boolean is true', () => {
-      const dom = HTMLParser.htmlToDom('<p><b selected?="true">Hey</b></p>')
+      const dom = HTMLParser.htmlToDom('<p><b :selected="true">Hey</b></p>')
       assert.equal(HTMLParser.parseAttributeBoolean(dom).innerHTML, '<p><b selected="selected">Hey</b></p>')
     })
     it('should remove attribute when boolean is false', () => {
-      const dom = HTMLParser.htmlToDom('<p><b selected?="false">Hey</b></p>')
+      const dom = HTMLParser.htmlToDom('<p><b :selected="false">Hey</b></p>')
       assert.equal(HTMLParser.parseAttributeBoolean(dom).innerHTML, '<p><b>Hey</b></p>')
     })
   })
 
   describe('#parseAttributeFor()', () => {
     it('should remove node when list is empty', () => {
-      const dom = HTMLParser.htmlToDom('<p><a for="${ url in this.urls }">link</a></p>')
+      const dom = HTMLParser.htmlToDom('<p><a :for="${ url in this.urls }">link</a></p>')
       assert.equal(HTMLParser.parseAttributeFor(dom).innerHTML, '<p></p>')
     })
     it('should duplicate nodes when list has elements', () => {
-      const dom = HTMLParser.htmlToDom('<p><a for="${ url in this.urls }">${ this.url }</a></p>')
+      const dom = HTMLParser.htmlToDom('<p><a :for="${ url in this.urls }">${ this.url }</a></p>')
       assert.equal(HTMLParser.parseAttributeFor(dom, { urls: ['localhost', 'example'] }).innerHTML, '<p><a>localhost</a><a>example</a></p>')
     })
   })
@@ -75,31 +75,31 @@ describe('HTMLParser', () => {
       assert.equal(HTMLParser.parse(template, { user: { name: 'John', age: 42 } }), 'John is 42 years old')
     })
     it('should evaluate template nested "if" attributes', () => {
-      const template = '<p><a if="${ this.showLink }">link</a><b if="${ this.showStrong }">strong</b></p>'
+      const template = '<p><a :if="${ this.showLink }">link</a><b :if="${ this.showStrong }">strong</b></p>'
       assert.equal(HTMLParser.parse(template, { showLink: true, showStrong: false }), '<p><a>link</a></p>')
     })
     it('should evaluate template boolean attribute', () => {
-      const template = '<p><a if="${ this.showLink }" selected?="${ this.isSelected }">link</a></p>'
+      const template = '<p><a :if="${ this.showLink }" :selected="${ this.isSelected }">link</a></p>'
       assert.equal(HTMLParser.parse(template, { showLink: true, showStrong: false, isSelected: true }), '<p><a selected="selected">link</a></p>')
     })
     it('should duplicate nodes when list has elements', () => {
-      const template = '<p><b><a for="${ url in this.urls }">${ this.url }</a></b></p>'
+      const template = '<p><b><a :for="${ url in this.urls }">${ this.url }</a></b></p>'
       assert.equal(HTMLParser.parse(template, { urls: ['localhost', 'example'] }), '<p><b><a>localhost</a><a>example</a></b></p>')
     })
     it('should not display "for" when "if" is false', () => {
-      const template = '<p>links: <a if="${ this.showLink }" for="${ url in this.urls }">${ this.url }</a></p>'
+      const template = '<p>links: <a :if="${ this.showLink }" :for="${ url in this.urls }">${ this.url }</a></p>'
       assert.equal(HTMLParser.parse(template, { showLink: false, urls: ['localhost', 'example'] }), '<p>links: </p>')
     })
     it('should loop "for" when "if" is true', () => {
-      const template = '<p>links: <a if="${ this.showLink }" for="${ url in this.urls }">${ this.url }</a></p>'
+      const template = '<p>links: <a :if="${ this.showLink }" :for="${ url in this.urls }">${ this.url }</a></p>'
       assert.equal(HTMLParser.parse(template, { showLink: true, urls: ['localhost', 'example'] }), '<p>links: <a>localhost</a><a>example</a></p>')
     })
   })
 
 
   describe('#registerListeners()', () => {
-    it('should remove an attribute with `on-` when converting into a listener', () => {
-      const dom = HTMLParser.htmlToDom('<p><a on-click="this.click">link</a></p>')
+    it('should remove an attribute with `:on-` when converting into a listener', () => {
+      const dom = HTMLParser.htmlToDom('<p><a :on-click="this.click">link</a></p>')
       assert.equal(HTMLParser.registerListeners(dom, { click: () => 'clicked' }).innerHTML, '<p><a>link</a></p>')
     })
   })
