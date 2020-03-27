e45fc2f3a6c724e13d78ad85b4b7e8f816f8f766
Vincent Agnano <vinyll@protonmail.com>
2020-03-27 12:39:28 +0100

    component is fully based on vddom with reactive state

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
 import { h, Component } from "./lib/Component.js";
-import { literal } from "./lib/parsers.js"
 
 class XApp extends Component {
   init() {
-    this.state = {
-      letters: [
-        { unit: 'a' },
-        { unit: 'b' },
-      ],
-      choice: 1,
-    }
+    this.letters = [
+      { unit: 'a', space: 2 },
+      { unit: 'b', space: 4 },
+    ]
+     this.state = {
+       choice: 1,
+       extra: ''
+     }
     setInterval(() => {
       this.state.choice = Number(!Boolean(this.state.choice))
       this.render()
     }, 500)
   }
 
+  over(target) {
+    this.state.extra = 'over!'
+  }
+
   get vdom() {
-    return ({ state }) => (
-      h('x-letter', { 'choice': state.choice, letter: state.letters[state.choice] })
-    )
+    return ({ state }) => ([
+      h('x-letter', { 'choice': state.choice, extra: state.extra, letter: this.letters[state.choice] }),
+      [...this.letters.map((letter, i) => h('x-letter', { choice: i, letter: letter }))],
+      ((state.choice === 1) ? h('x-letter', { choice: 1, letter: this.letters[1] }) : ''),
+      h('footer', {}, 'end!')
+    ])
   }
 }
 
 class XLetter extends Component {
+  init() {
+    this.state = {
+      letter: {},
+      extra: '…',
+    }
+    this._props = Object.assign({}, this.state)
+  }
+
+  click(event) {
+    this.state.extra = 'clicked!'
+    this.render()
+  }
+
   get vdom() {
     return ({ state }) => ([
-      h('p', {}, `Hey ${state.choice} ${state.letter.unit}`)
+      h('p', { 'onclick': this.click.bind(this) }, [
+        h('span', {}, `Hey ${this.getAttribute('choice')}`),
+        `${state.letter.unit} - ${state.extra}`,
+      ]),
+      (state.choice === 1 ? h('aside', {}, '1 is good') : ''),
     ])
+  }
+
+  get vstyle() {
+    return ({ state }) => h('style', {}, `span { font-weight: bold; padding-left: ${state.letter.space}rem }`)
   }
 }
 
