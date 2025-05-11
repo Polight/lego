cc73dd86d20518ad5f1ba85e28a92be17de2cb1b
Mlbiche <tmlb@pm.me>
2025-05-11 17:52:14 +0200

    fix: minor fixes

--- src/lib/Component.js
+++ src/lib/Component.js
@@ -78,7 +78,6 @@ class Component extends HTMLElement {
 
   setState(props = {}) {
     Object.assign(this.state, props)
-    if (this.changed && this.#isConnected) this.changed(props)
   }
 
   set state(value) {
@@ -90,13 +89,15 @@ class Component extends HTMLElement {
   }
 
   render(state) {
-    this.setState(state)
     if (!this.#isConnected) return
+    if (state) this.setState(state)
 
-    return render(
+    render(
       [this.vdom({ state: this.state }), this.vstyle({ state: this.state })],
       this.document
     )
+
+    this.changed?.(state)
   }
 }
 
